108d9bd4be0e9bc07070a63e731d72c37c224249
3wguap <rnbrell@ya.ru>
2025-08-02 16:15:03 +0400

    chekstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmDbStorage.java
@@ -120,27 +120,25 @@ public class FilmDbStorage implements FilmStorage {
                 GROUP BY f."id", f."name", f."description", f."release_date", f."duration",\s
                          m."id", m."name", m."description"
                 ORDER BY "likes_count" DESC;
-            
             """;
     private static final String GET_COMMON_FILMS_BY_USERS_SORTED_BY_POPULARITY = """ 
             SELECT f."id",
-                   f."name",
-                   f."description",
-                   f."release_date" AS "releaseDate",
-                   f."duration",
-                   m."id" AS "mpa_id",
-                   m."name" AS "mpa_name",
-                   m."description" AS "mpa_description",
-                   COUNT(l."user_id") AS likes_count
-            FROM "films" f
-            JOIN "likes" l1 ON f."id" = l1."film_id" AND l1."user_id" = ?
-            JOIN "likes" l2 ON f."id" = l2."film_id" AND l2."user_id" = ?
-            LEFT JOIN "likes" l ON f."id" = l."film_id"
-            LEFT JOIN "mpa_rating" m ON f."mpa_id" = m."id"
-            GROUP BY f."id", f."name", f."description", f."release_date", f."duration",\s
-                     m."id", m."name", m."description"
-            ORDER BY likes_count DESC;
-            
+                    f."name",
+                    f."description",
+                    f."release_date" AS "releaseDate",
+                    f."duration",
+                    m."id" AS "mpa_id",
+                    m."name" AS "mpa_name",
+                    m."description" AS "mpa_description",
+                    COUNT(l."user_id") AS likes_count
+             FROM "films" f
+             JOIN "likes" l1 ON f."id" = l1."film_id" AND l1."user_id" = ?
+             JOIN "likes" l2 ON f."id" = l2."film_id" AND l2."user_id" = ?
+             LEFT JOIN "likes" l ON f."id" = l."film_id"
+             LEFT JOIN "mpa_rating" m ON f."mpa_id" = m."id"
+             GROUP BY f."id", f."name", f."description", f."release_date", f."duration",\s
+                      m."id", m."name", m."description"
+             ORDER BY likes_count DESC;
             """;
 
 
